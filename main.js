376a2ae04b0d5644f5a3dd027a5f3846112161cf
VEdant732 <VedantAjay377@gmail.com>
2024-10-03 20:50:44 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+ function showspan1(){
+    window.alert("Machu Picchu is a 15th-century Inca citadel located in the Eastern Cordillera of southern Peru on a mountain ridge at 2,430 meters (7,970 ft).[9] Often referred to as the Lost City of the Incas,[10] it is the most familiar icon of the Inca Empire. It is located in the Machupicchu District within Urubamba Province[11] above the Sacred Valley, which is 80 kilometers (50 mi) northwest of Cusco. The Urubamba River flows past it, cutting through the Cordillera and creating a canyon with a subtropical mountain climate.[12]");
+ }
+ function showspan2(){
+    window.alert("Christ the Redeemer (Portuguese: Cristo Redentor, standard Brazilian Portuguese: [kɾistu ʁedẽtoʁ]) is an Art Deco statue of Jesus in Rio de Janeiro, Brazil, created by French-Polish sculptor Paul Landowski and built by Brazilian engineer Heitor da Silva Costa, in collaboration with French engineer Albert Caquot. Romanian sculptor Gheorghe Leonida sculpted the face. Constructed between 1922 and 1931, the statue is 30 metres (98 ft) high, excluding its 8-metre (26 ft) pedestal. The arms stretch 28 metres (92 ft) wide.[1][2] It is made of reinforced concrete and soapstone.[3][4][5] Christ The Redeemer differs considerably from its original design, as the initial plan was a large Christ with a globe in one hand and a cross in the other. Although the project organizers originally accepted the design, it later changed to the statue of today, with the arms spread out wide.")
+ }
+ function showspan3(){
+    window.alert("The Taj Mahal is an ivory-white marble mausoleum on the right bank of the river Yamuna in Agra, Uttar Pradesh, India. It was commissioned in 1631")
+ }
+ function showspan4(){
+    window.alert("The Great Pyramid of Giza is the largest Egyptian pyramid. It served as the tomb of pharaoh Khufu, who ruled during the Fourth Dynasty of the Old Kingdom.")
+ }
+ function showspan5(){
+    window.alert("The Great Wall of China is a series of fortifications that were built across the historical northern borders of ancient Chinese states and Imperial China ")
+ }
+ function showspan6(){
+    window.alert("The Colosseum is an elliptical amphitheatre in the centre of the city of Rome, Italy, just east of the Roman Forum. It is the largest ancient amphitheatre ")
+}
